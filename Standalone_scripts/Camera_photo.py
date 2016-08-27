9fe0615213ea282df166f2635daacb5b7c51c441
Den4iks <lol4ik99@gmail.com>
2016-08-27 09:50:06 +0000

    add connect to wifi method

--- Standalone_scripts/Camera_photo.py
+++ Standalone_scripts/Camera_photo.py
@@ -6,14 +6,16 @@
 import os, re, sys, time, socket, requests
 from settings import camaddr
 from settings import camport
+from wifi import Cell, Scheme
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 
 def main():
-    bot_initializer()
-
-
+     bot_initializer()
+     connect_to_wifi()
+##     create_wifi_scheme()
+#    bot_initializer()
 #    data = make_photo()
 #    pathtophoto = re.findall('fuse_d(.+?)"', data)[0]
 #    downloadphoto(pathtophoto)
@@ -45,26 +47,35 @@ def make_photo():
         responsedata = srv.recv(512)
     return responsedata
 
+def connect_to_wifi(name):
+     scheme = Scheme.find('wlan0',name)
+     scheme.activate()
+
+def create_wifi_scheme(name,password):
+##     cell = Cell.all('wlan0')[0]
+##     scheme = Scheme.for_cell('wlan0','YDXJ_5327678', cell, '1234567890')
+##     scheme.save()
 
 def downloadphoto(path):
     filename = re.findall('(?:.*?\/){3}(.*)', path)[0]
     url = "http://" + camaddr + path
     r = requests.get(url, stream=True)
     if r.status_code == 200:
-        with open("F:\\" + filename, 'wb') as f:
+        with open("/tmp/photo/" + filename, 'wb') as f:
             for chunk in r.iter_content(1024):
                 f.write(chunk)
 
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, to make photo please print !")
 
 
 def echo(bot, update):
-    if 'Hi' in update.message.text:
-     bot.sendPhoto(chat_id=update.message.chat_id, photo=open('F:\YDXJ0788.jpg', 'rb'))
-    else:
-        bot.sendMessage(chat_id=update.message.chat_id, text="Privet")
+     bot.sendMessage(chat_id=update.message.chat_id, text="")
+     if 'Hi' in update.message.text:
+          bot.sendPhoto(chat_id=update.message.chat_id, photo=open('F:\YDXJ0788.jpg', 'rb'))
+     else:
+          bot.sendMessage(chat_id=update.message.chat_id, text="Privet")
 
 
 def caps(bot, update, args):
